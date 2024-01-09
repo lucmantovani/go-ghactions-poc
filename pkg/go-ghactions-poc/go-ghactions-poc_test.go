9daad7f0c0fb1a1c5a102de902df66bc92839479
Lucas Mantovani <lucas.mantovani@mercadolivre.com>
2024-01-09 15:44:08 -0300

    Update go-ghactions-poc_test.go

--- pkg/go-ghactions-poc/go-ghactions-poc_test.go
+++ pkg/go-ghactions-poc/go-ghactions-poc_test.go
@@ -1,14 +1,1 @@
 package goghactionspoc
-
-import "testing"
-
-func TestHello(t *testing.T) {
-	g := GoGHActionsPOC{}
-	g.Hello()
-	t.Fatal("Hello World!")
-}
-
-func TestHello2(t *testing.T) {
-	g := GoGHActionsPOC{}
-	g.Hello()
-}
