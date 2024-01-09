f0f1f1719a7d08a4ce0caa8c9e90d3d071b69761
Lucas Mantovani <lucas.mantovani@mercadolivre.com>
2024-01-09 16:24:21 -0300

    Update go-ghactions-poc.go

--- pkg/go-ghactions-poc/go-ghactions-poc.go
+++ pkg/go-ghactions-poc/go-ghactions-poc.go
@@ -9,7 +9,7 @@ type GoGHActionsPOC struct{}
 
 func (g *GoGHActionsPOC) Hello() {
 	var m string
-	err := json.Unmarshal([]byte("\"Hello World!\""), &m)
+	err := json.Unmarshal([]byte("Hello World!"), &m)
 	if err != nil {
 		fmt.Println(err)
 	}
